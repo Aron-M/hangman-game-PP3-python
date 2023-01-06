359378101d862f227f7fd67bc048ba74609fc9b4
Aron-M <aron.moelich88@gmail.com>
2023-01-06 15:07:04 +0000

    adding functionality for guessinng letters in each random word

--- hangman.py
+++ hangman.py
@@ -46,3 +46,15 @@ guessed_correctly = []
 guessed_incorrectly = []
 tries = 6
 hangman_count = -1
+
+while tries > 0:
+    output = ''
+    for letter in word:
+        if letter in guessed_correctly:
+            output += letter
+        else:
+            output += '_ '
+    if output == word:
+        break
+    print("Guess the word: ",output)
+    print(tries," chances left")
